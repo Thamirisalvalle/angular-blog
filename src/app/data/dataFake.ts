869d421d9cb5c089cc931b5ac188521174f360b5
Thamiris Alvalle <thamirisalvalle@gmail.com>
2023-10-16 19:58:25 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id": "2",
+    "title": "Nova série anunciada no Disney +",
+    "description": "Ola Bla Bla Bla Bla Bla Bla",
+    "photoCover": "https://nosbastidores.com.br/wp-content/uploads/2021/08/Series-Marvel-Disney-Plus-1024x576-1.jpg"
+  }
+]
